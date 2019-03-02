1595c9c2092cb8d8f7e8bf2d6e1f17d1406d3c85
rashedsahaji <asifrashed2@gmail.com>
2019-03-02 19:37:15 +0530

    Make it official

--- device.mk
+++ device.mk
@@ -292,6 +292,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# OTA Updates
+PRODUCT_PACKAGES += \
+    Updates
+
 # Keymaster HAL
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
