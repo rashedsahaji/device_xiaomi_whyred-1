0aa7ab826ce63d5f2818a0f0d42eaf841e7f7857
HeartStealer786 <zeusmj786@gmail.com>
2019-06-13 21:40:29 +0530

    Stag-ify

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
-$(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/whyred.mk)
+$(call inherit-product, vendor/xiaomi/MiuiCamera/whyred.mk)
 
 # Inherit from whyred device
 $(call inherit-product, device/xiaomi/whyred/device.mk)
@@ -26,13 +26,13 @@ $(call inherit-product, device/xiaomi/whyred/device.mk)
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/stag/main.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_BOOT_ANIMATION_RES_EVO := true
+
 TARGET_GAPPS_ARCH := arm64
 CUSTOM_BUILD_TYPE :=  OFFICIAL
-PRODUCT_NAME := aosp_whyred
+PRODUCT_NAME := stag_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
